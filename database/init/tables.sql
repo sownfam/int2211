4624b662e3ef7231c61f7f9a4ef54879971249c1
sownfam <ptson.0403@gmail.com>
2023-11-01 15:18:26 +0700

    feat: update database schema

--- database/init/tables.sql
+++ database/init/tables.sql
@@ -1,15 +1,49 @@
-drop database if exists `mytable`;
-create database `mytable`;
-use `mytable`;
+drop database if exists `int2211-blog`;
+create database `int2211-blog`;
+use `int2211-blog`;
+
+drop table if exists topic;
+
+create table `topic` (
+  `topicID` int not null AUTO_INCREMENT,
+  `name` varchar(50) not null,
+  primary key (`topicID`)
+);
+
+insert into topic(`name`) values
+('Backend'),
+('Frontend'),
+('Cloud'),
+('Database');
+
+drop table if exists user;
+
+create table `user` (
+  `userID` varchar(50) not null,
+  `username` varchar(50) not null,
+  `password` varchar(50) not null,
+  primary key (`userID`)
+);
 
 drop table if exists blog;
 
 create table `blog` (
-  `id` varchar(50) not null,
+  `id` int not null AUTO_INCREMENT,
   `slug` varchar(50),
   `title` varchar(50),
-  `categories` varchar(100),
   `cover` varchar(512) character set 'ascii' collate 'ascii_general_ci',
   `content` text,
-   primary key (`id`)
+  `author` varchar(50) not null,
+  primary key (`id`),
+  foreign key(`author`) references user(`userID`)
+);
+
+drop table if exists blog_topic;
+
+create table `blog_topic` (
+  `blogID` int not null,
+  `topicID` int not null,
+  primary key (`blogID`, `topicID`),
+  foreign key (`blogID`) references blog(`id`),
+  foreign key (`topicID`) references topic(`topicID`)
 );
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,15 +1,17 @@
 async function getBlogContent(slug: string) {
-  const searchParams = new URLSearchParams();
-  searchParams.append("slug", slug);
-  const fetchPath =
-    "http://localhost:3000" +
-    "/api/query-blog-content" +
-    `?${searchParams.toString()}`;
+  const mockContent = "<div> Hello World </div>";
+  // const searchParams = new URLSearchParams();
+  // searchParams.append("slug", slug);
+  // const fetchPath =
+  //   "http://localhost:3000" +
+  //   "/api/query-blog-content" +
+  //   `?${searchParams.toString()}`;
   
-  const response = await fetch(fetchPath);
-  const body = await response.text();
-  const data = JSON.parse(body);
-  return data.posts[0][0].content;
+  // const response = await fetch(fetchPath);
+  // const body = await response.text();
+  // const data = JSON.parse(body);
+  // return data.posts[0][0].content;
+  return mockContent;
 }
 
 export default async function BlogContent({
