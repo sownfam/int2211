4680a5d8fd7214f08ad0f5033f03349b5a327b57
sownfam <ptson.0403@gmail.com>
2023-11-01 19:56:36 +0700

    feat: minimal user login

--- src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Contact',
-  description: 'This is contact page.',
-};
-
-export default function ContactPage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">Contact</h1>;
-}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,22 @@
+'use client'
 import Link from 'next/link';
 
 import NavLink from '@/components/header/nav-link';
+import { useRecoilValue } from 'recoil';
+import { userAuth } from '@/states/user';
 
 const NAV_ITEMS = [
-  { path: 'blog', name: 'Blog' },
-  { path: 'about', name: 'About' },
-  { path: 'contact', name: 'Contact' },
+  { path: 'blog', name: 'Explore' },
   { path: 'create', name: 'Create Blog' },
+  { path: 'login', name: 'Login' },
 ];
 
 export default function Header() {
+  const { username } = useRecoilValue(userAuth);
   return (
     <nav className="flex flex-col justify-between py-12 md:flex-row">
       <Link href="/" className="self-start md:self-auto">
-        <h1 className="text-3xl font-bold">Admin</h1>
+        <h1 className="text-3xl font-bold"> {username ? `Welcome, ${username}` : 'Guest'} </h1>
       </Link>
       <div className="my-6 flex space-x-8 self-center md:my-0 md:self-auto">
         <ul className="flex space-x-8">
