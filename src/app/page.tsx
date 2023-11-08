53cd4e1f443e36e76a156e981ea57c4519e9407e
sownfam <ptson.0403@gmail.com>
2023-11-08 20:37:25 +0700

    fix: refine user session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@
 import PostsGrid from "@/components/posts/posts-grid";
 import { userAuth } from "@/states/user";
 import { Post } from "@/types/post";
+import React from "react";
 import { useRecoilValue } from "recoil";
 
-async function getData() {
-  const { userID } = useRecoilValue(userAuth);
+async function getData(userID: string) {
   console.log("???? UID: ", userID);
-  const fetchResponse = await fetch(`http://localhost:3000/api/query-user-posts?userID=${userID}`, {cache: 'no-store'});
+  const fetchResponse = await fetch(`http://localhost:3000/api/query-user-posts?userID=${userID}`, {cache: 'no-cache'});
   const body = await fetchResponse.text();
   const queryPostsResponse = JSON.parse(body);
-  console.log("All Posts response: ", queryPostsResponse.posts[0]);
+  console.log("All Posts response at home: ", queryPostsResponse.posts[0]);
   const allPosts = queryPostsResponse.posts[0] as any[];
   const realPosts: Post[] = allPosts.map(({topic, ...others}) => {
     topic = topic.split(',');
@@ -20,8 +20,16 @@ async function getData() {
   return realPosts;
 }
 
-export default async function HomePage() {
-  const allPosts = await getData();
+export default function HomePage() {
+  const { userID } = useRecoilValue(userAuth);
+  const [allPosts, setAllPosts] = React.useState<Post[]>([]);
+  React.useEffect(() => {
+    async function fetchHomePage() {
+      const allPosts = await getData(window.localStorage.getItem("blog-userID") ?? userID);
+      setAllPosts(allPosts);
+    }
+    fetchHomePage();
+  }, [])
   return (
     <PostsGrid allPosts={allPosts} />
   );
